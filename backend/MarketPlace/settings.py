29eb2f044d702c08069f00ea7d8f721550dd2aa7
Pal-cloud <paloma.gom.sal@gmail.com>
2026-01-21 12:57:27 +0100

    build: add new dependencies to requirements.txt

--- backend/MarketPlace/settings.py
+++ backend/MarketPlace/settings.py
@@ -13,9 +13,8 @@ ALLOWED_HOSTS = ["*"]
 
 DATABASES = {
     "default": dj_database_url.parse(
-        os.getenv("DATABASE_URL"),
-        conn_max_age=600,
-        ssl_require=True
+        os.getenv("DATABASE_URL", "sqlite:///db.sqlite3"),
+        conn_max_age=600
     )
 }
 
